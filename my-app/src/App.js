f87a5f1380ad2a44a02687fc3d1d165a2c31a212
Aditya Singh <aditya.l.k.2001@gmail.com>
2023-06-02 02:31:16 +0530

    Navbar and home pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import * as Components from './Components/index'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Header
+        <Components.Navbar/>
       </header>
     </div>
   );
