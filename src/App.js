70aad3857b69fafd761805e4454277bb61f996f0
ArnavPrakash <arnavprakash@iitbhilai.ac.in>
2023-06-04 22:46:46 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import './App.css';
-import * as Components from './Components/index'
-import * as Pages from './Pages/index'
+import "./App.css";
+import * as Components from "./Components/index";
+import * as Pages from "./Pages/index";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Components.Navbar />}>
-        <Route index element={<Pages.Home />} />
-        <Route path="/Product" element={<Pages.Product />} />
-        <Route path="/AboutUs" element={<Pages.Aboutus />} />
-        <Route path="/Contact" element={<Pages.Contact />} />
-        <Route path="/Catalog" element={<Pages.Catalog />} />
-        <Route path="/Testimonial" element={<Pages.Testimonial />} />
-        <Route path="/Gallery" element={<Pages.Gallery />} />
-        <Route path="/Login" element={<Pages.Login />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Components.Navbar /> <Components.Footer />
+              </>
+            }
+          >
+            <Route index element={<Pages.Home />} />
+            <Route path="/Product" element={<Pages.Product />} />
+            <Route path="/AboutUs" element={<Pages.Aboutus />} />
+            <Route path="/Contact" element={<Pages.Contact />} />
+            <Route path="/Catalog" element={<Pages.Catalog />} />
+            <Route path="/Testimonial" element={<Pages.Testimonial />} />
+            <Route path="/Gallery" element={<Pages.Gallery />} />
+            <Route path="/Login" element={<Pages.Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
