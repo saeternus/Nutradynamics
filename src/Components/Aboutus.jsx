a2c1aab06b0ea2904245a2253cf5a7ba2c212b9e
Aditya Singh <aditya.l.k.2001@gmail.com>
2023-06-04 13:27:28 +0530

    Completed Routing and added Aboutus page

---
+++ src/Components/Aboutus.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Styles/Aboutus.css'
+
+function Aboutus() {
+    return (
+        <div className='Aboutbox'>
+            <div className='aboutdes'>
+                <div className='Abouttext'>About Us</div>
+                <div className='aboutdestext'>Lörem ipsum föna e-learning triskade, nyrtad
+                    , sobelt. Nint doling, möre viment oning. Kanet
+                    öktig a inde. Saföse dock nyn har prerespel ja
+                    g industrisafari. Vis a diare Epiliga hiliga ultras
+                    0sa alltså vabåsk nerar. Ön semir när prenat d
+                    ikarta d med exotid inte transperson Mära köttr
+                    ymden antegt risamma. Gigalig ambifiering tr
+                    eliga. Sup  rakroheten prertad sälogi att tibelt.
+                    Lörem ipsum föna e-learning triskade, nyrtad
+                    , sobelt. Nint doling, möre viment oning. Kanet
+                    öktig a inde. Saföse dock nyn har prerespel ja
+                    g industrisafari. </div>
+            </div>
+            <div className='aboutimg'></div>
+        </div>
+    )
+}
+
+export default Aboutus
--- src/Pages/Aboutus.jsx
+++ src/Pages/Aboutus.jsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import * as Components from '../Components/index'
+import './Styles/Aboutus.css'
+import person from '../Assets/person.png'
+import leftimg from '../Assets/Expand_left_light.png'
+import rightimg from '../Assets/Expand_right_light.png'
 
 function Aboutus() {
+  const dummydata = {
+    Team:
+      [{
+        name: 'Jhon Wick',
+        post: 'Creative Director',
+        img: `${person}`
+      }, {
+        name: 'Jhon Wick',
+        post: 'Creative Director',
+        img: `${person}`
+      }, {
+        name: 'Jhon Wick',
+        post: 'Creative Director',
+        img: `${person}`
+      }, {
+        name: 'Jhon Wick',
+        post: 'Creative Director',
+        img: `${person}`
+      }, {
+        name: 'Jhon Wick',
+        post: 'Creative Director',
+        img: `${person}`
+      }, {
+        name: 'Jhon Wick',
+        post: 'Creative Director',
+        img: `${person}`
+      }, {
+        name: 'Jhon Wick',
+        post: 'Creative Director',
+        img: `${person}`
+      }, {
+        name: 'Jhon Wick',
+        post: 'Creative Director',
+        img: `${person}`
+      },]
+  }
+  let y = dummydata.Team.length
+  let x
+  const ref = useRef(null)
+  const [blockleft, setblockleft] = useState(true)
+  const [blockright, setblockright] = useState(false)
+  const [left, setleft] = useState(0)
+  const [width, setwidth] = useState(0)
+
+  useLayoutEffect(() => {
+    setwidth(ref.current.offsetWidth)
+  }, [left])
+  function moveright() {
+    x = left - 400
+    if (x - width < -(y * 400)) {
+      setleft(-(y * 400) + width)
+      setblockright(true)
+    }
+    else {
+      setleft(x)
+      setblockleft(false)
+    }
+  }
+  function moveleft() {
+    x = left + 400
+    if (x > 0) {
+      setleft(0)
+      setblockleft(true)
+    }
+    else {
+      setleft(x)
+      setblockright(false)
+    }
+  }
+
   return (
-    <div>
-      About Us
+    <div className='center'>
+      <Components.Aboutus />
+      <div className='teambox'>
+        <div className='teamheading'>Team</div>
+        <div className='teamgroup'>
+          <div className='arrow' onClick={moveleft} disabled={blockleft}>
+            <img src={leftimg} alt="" />
+          </div>
+          <div className='teamgroupinner' ref={ref}>
+            <div className='insideteam' id='wrapper' style={{ transform: `translateX(${left}px)` }}>
+              {dummydata.Team.map((member)=>{
+                console.log(member.img)
+                return <Components.Teamcard name={member.name} title={member.post} img={member.img} />
+              })}
+            </div>
+          </div>
+          <div className='arrow' onClick={moveright} disabled={blockright}>
+            <img src={rightimg} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
